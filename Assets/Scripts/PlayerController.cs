0e2601cbafb0904259e1153991257cca3c96770a
Scott Isitt <isitts@hotmail.com>
2022-05-02 01:31:18 -0700

    Create boundaries in viewport to restrict Player to screen view.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 moveInput;
     [SerializeField] float moveSpeed;
 
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
 
+    Vector2 moveInput;
+
+    Vector2 minBoundary;
+    Vector2 maxBoundary;
+
+    void Start()
+    {
+        InitBoundaries();    
+    }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMove();
+    }
+
+
+    void InitBoundaries()
+    {
+        Camera mainCamera = Camera.main;
+        minBoundary = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBoundary = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
     }
 
 
@@ -25,7 +45,10 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMove()
     {
-        Vector3 delta = moveInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = moveInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBoundary.x + paddingLeft, maxBoundary.x -paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBoundary.y + paddingBottom, maxBoundary.y - paddingTop);
+        transform.position = newPos;
     }
 }
