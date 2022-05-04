4010ce0eb1f83ff9bcba1d585a763dbb51f9f04b
Scott Isitt <isitts@hotmail.com>
2022-05-03 23:52:26 -0700

    Add getter methods for Enemy prefab number and Enemy prefab index in WaveConfigSO.

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "WaveConfig", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+
+
+    public int GetEnemyCount()
+    {
+        Debug.Log(enemyPrefabs.Count);
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
+
 
     public Transform GetStartingWaypoint()
     {
